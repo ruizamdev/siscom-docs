7b134ccf785d9e97a437ea11c3705ac7d4ff9b73
Armando Ruiz <artmx@proton.me>
2025-11-23 22:35:43 -0600

    ready to deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
-import {useEffect} from 'react';
 import {Redirect} from '@docusaurus/router';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function Home() {
-  return <Redirect to="/docs/intro" />;
+  const docsUrl = useBaseUrl('/docs/intro');
+  return <Redirect to={docsUrl} />;
 }
